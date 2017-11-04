7cfeb530cb0470f2299bae9d0f1b64b3d424f747
Juan Du <juandu@JuandeMacBook-Air.local>
2017-11-04 11:34:52 +0800

    add function

--- handler.js
+++ handler.js
@@ -1,11 +1,22 @@
 'use strict';
 
 module.exports.hello = (event, context, callback) => {
+  var statusCode = 200;
+  var result;
+
+  if (isNaN(event.num1) || isNaN(event.num2)) {
+    statusCode = 500;
+    result = "Not a number.";
+  } else {
+    result = parseInt(event.num1) + parseInt(event.num2);
+  }
+  
   const response = {
-    statusCode: 200,
+    statusCode: statusCode,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
       input: event,
+      result: result,
     }),
   };
 
